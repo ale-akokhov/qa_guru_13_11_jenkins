8fcd170a0339b9b621b0417a271f4ab2bc8413a7
Alexander <ale-akokhov@ayndex.ru>
2026-06-06 17:20:10 +0300

    Telegram config added

--- src/test/java/tests/simple/SkippedTests.java
+++ src/test/java/tests/simple/SkippedTests.java
@@ -1,10 +1,12 @@
 package tests.simple;
 
 import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
+@Tag("simple")
 public class SkippedTests {
 
     @Test
